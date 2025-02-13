33c843075e522d108ca18f19a5e0c164f4f3ca31
MostafaZayed <mostafa.zayed@hypercell.net>
2025-02-13 17:37:17 +0200

    add user entity

---
+++ book.network/src/main/java/com/mostafa/book/network/user/User.java
@@ -0,0 +1,93 @@
+package com.mostafa.book.network.user;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.security.Principal;
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "_user")
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+public class User implements UserDetails, Principal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+    private boolean accountLocked;
+    private boolean enabled;
+
+
+    @Column(nullable = false,updatable = false)
+    @CreatedDate
+    private LocalDate createdAt;
+
+    @Column(insertable = false)
+    @LastModifiedDate
+    private LocalDate lastUpdatedAt;
+
+    @Override
+    public String getName() {
+        return email;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return accountLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    private String getFullName(){
+        return firstName + " " + lastName;
+     }
+}
+
